d7c44cfc122b4ceae5dd0271f46c4825f64fd547
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-07 14:05:07 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -18,6 +18,7 @@ def change_rainbow_colors
 
 end
 change_rainbow_colors
+
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 end
